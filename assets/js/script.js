5ea2e4a88a419af0ed252e5e8ee3972868ebf71b
Azizmemon786 <136925916+Azizmemon786@users.noreply.github.com>
2024-05-08 18:23:43 +0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,26 @@ var swiper = new Swiper(".logoSwiper", {
   autoplay: {
     delay: 5000,
   },    
-  });
+  breakpoints: {
+    // when window width is >= 320px
+    275: {
+      slidesPerView: 1,
+      spaceBetween: 74,
+    },
+    575: {
+      slidesPerView: 4,
+      spaceBetween: 74,
+    },
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 74,
+    },
+
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 74,
+    },
+}});
 
 
   var swiper = new Swiper(".newsSwiper", {
@@ -13,8 +32,17 @@ var swiper = new Swiper(".logoSwiper", {
     // autoplay: {
     //     delay: 3000,
     //   },    
+    breakpoints: {
+    275: {
+      slidesPerView: 1,
+      spaceBetween: 74,
+    },
+    575: {
+      slidesPerView: 3,
+      spaceBetween: 74,
+    },
     navigation: {
       nextEl: ".news-next",
       prevEl: ".news-prev",
     },
-  });
+  }});
